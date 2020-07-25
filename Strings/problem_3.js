cfae36c14d398d4476916d203605bb226e1e4b37
Kevin Apostol <apostolkevin@gmail.com>
2020-07-24 21:19:20 -0700

    Splits the string and converts it to an array

---
+++ Strings/problem_3.js
@@ -0,0 +1,7 @@
+// Write a JavaScript function to split a string and convert it into an array of words.
+
+function string_to_array( string ) {
+    return string.split( ' ' );
+}
+
+console.log( string_to_array( "Robin Singh" ) );
